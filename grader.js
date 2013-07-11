372eb8e2d5ecbc762b0f0791410ab9bc942ff9c3
Francisco Villasanti <villasanti.francisco@gmail.com>
2013-07-11 20:07:56 +0000

    ultima actualizacion hw3-3

--- grader.js
+++ grader.js
@@ -1,30 +1,55 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var HTMLURL_DEFAULT = "http://shielded-wildwood-6395.herokuapp.com/";
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
-    if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
-    }
-    return instr;
+              if(!fs.existsSync(instr)) {
+                  console.log("%s does not exist. Exiting.", instr);
+                  process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code                                                                         
+                 }
+                    return instr;
 };
 
+
+
 var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+        return cheerio.load(fs.readFileSync(htmlfile));
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
+
+var checkHtmlURL = function(urlfile, checksfile) {
+        console.log("entra en url");            
+   $ = cheerio.load(urlfile);
+
+                  var checks = loadChecks(checksfile).sort();
+                   var out = {};
+                  for(var ii in checks) {
+
+                  var present = $(checks[ii]).length > 0;
+                  out[checks[ii]] = present;
+                  }
+
+                  return out;
+     
+};
+
+
+
 var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+     console.log("entra en file");
+     $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -44,10 +69,23 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+        .option('-u, --url <url_dir>','url to index.html','')
+         .parse(process.argv);
+      
+       if(program.url){
+          rest.get(program.url).on('complete', function(result){
+               var checkJson = checkHtmlURL(result, program.checks);
+               var outJson = JSON.stringify(checkJson, null, 4);
+               console.log(outJson);
+               
+           });
+
+       }else{
+            var checkJson = checkHtmlFile(program.file, program.checks);
+            var outJson = JSON.stringify(checkJson, null ,4);
+            console.log(outJson);
+       }
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
